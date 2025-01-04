7d36f95353bc7db167d1ea13812ee94ce9aed971
duhyunkim <duhyun.dev@gmail.com>
2025-01-04 19:39:24 +0900

    initialize

---
+++ ppartTests/ppartTests.swift
@@ -0,0 +1,10 @@
+import Testing
+@testable import ppart
+
+struct ppartTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
